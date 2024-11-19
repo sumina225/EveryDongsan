6f407e00083a05d382a32e2a5f98768415b25c47
sumin <psm5927@naver.com>
2024-11-19 23:19:04 +0900

    default

--- EveryDongSan/src/main/java/com/dongsan/member/service/MemberDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.dongsan.member.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface MemberDetailsService {
-
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-}
